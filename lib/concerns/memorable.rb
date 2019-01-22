4626bb22452c644acfaaffb5db827d73a893b8c8
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-22 02:40:52 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,9 @@
-module Memorable
-    def self.reset_all
-      self.all.clear
-    end
-
-    def self.count
-      self.all.count
-    end
-end 
+#module Memorable
+#    def self.reset_all
+#      self.all.clear
+#    end
+#
+#    def self.count
+#      self.all.count
+#    end
+# end 
