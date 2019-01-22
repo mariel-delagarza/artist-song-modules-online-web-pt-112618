92501e5f67d9cac64284bd4d18c6d1f5a3470971
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-22 02:54:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  extend Findable::ClassMethods 
+  extend Findable::ClassMethods
   include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
